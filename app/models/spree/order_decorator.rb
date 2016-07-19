6c7d824f02b8b635caa07b2346663c04c93323f9
Jake Bell <jake@theunraveler.com>
2016-07-19 13:50:17 -0500

    Sending on order:paid

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,7 +1,5 @@
 Spree::Order.class_eval do
-  self.state_machine.after_transition to: :complete, do: :create_3plcentral_order
-
-  private
+  self.state_machine.after_transition to: :paid, do: :create_3plcentral_order
 
   def create_3plcentral_order
     shipments.send_to_3plcentral.each(&:send_to_3plcentral)
