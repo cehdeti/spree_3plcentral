44d20a47c8335dd4d8ff650117c483a860e448d1
Jake Bell <jake@theunraveler.com>
2016-11-17 16:04:27 -0600

    Only send orders to 3plcentral if the order state changes to paid

--- app/models/spree/order_updater_decorator.rb
+++ app/models/spree/order_updater_decorator.rb
@@ -1,15 +1,16 @@
 Spree::OrderUpdater.class_eval do
-
-  def create_3plcentral_order
-    order.shipments.send_to_3plcentral.each(&:send_to_3plcentral) if order.payment_state == 'paid'
-  end
-
   def update_payment_state_with_create_3plcentral_order
+    initial_state = order.payment_state
     result = update_payment_state_without_create_3plcentral_order
-    create_3plcentral_order
+    create_3plcentral_order if initial_state != 'paid' && order.payment_state == 'paid'
     result
   end
 
   alias_method_chain :update_payment_state, :create_3plcentral_order
 
+  private
+
+  def create_3plcentral_order
+    order.shipments.send_to_3plcentral.each(&:send_to_3plcentral)
+  end
 end
