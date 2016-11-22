c18799e175140906ee344dae0f6cd4eca5dd1b0e
Jake Bell <jake@theunraveler.com>
2016-11-22 10:32:40 -0600

    More logging, using `Module#prepend`

--- app/models/spree/order_updater_decorator.rb
+++ app/models/spree/order_updater_decorator.rb
@@ -1,24 +1,28 @@
-Spree::OrderUpdater.class_eval do
-  def update_payment_state_with_create_3plcentral_order
+module ThreePLCentralOrderUpdater
+  def update_payment_state
     logger.tagged('3PLCentral', "Order ##{order.number}") do
       initial_state = order.payment_state
       logger.debug("Current order state: #{initial_state}")
-      update_payment_state_without_create_3plcentral_order.tap do
+      super.tap do
         logger.debug("New order state: #{order.payment_state}")
         create_3plcentral_order if initial_state != 'paid' && order.payment_state == 'paid'
       end
     end
   end
 
-  alias_method_chain :update_payment_state, :create_3plcentral_order
-
   private
-
-  def create_3plcentral_order
-    order.shipments.send_to_3plcentral.each(&:send_to_3plcentral)
-  end
 
   def logger
     order.try(:logger) || Rails.logger
   end
+
+  def create_3plcentral_order
+    logger.debug("#{__method__} with #{order.shipments.send_to_3plcentral.count} shipments")
+    order.shipments.send_to_3plcentral.each(&:send_to_3plcentral)
+    logger.debug("Done with #{__method__}")
+  end
+end
+
+Spree::OrderUpdater.class_eval do
+  prepend ThreePLCentralOrderUpdater
 end
