a023391d1d16565494bf4921854a1d13d39d7c65
Jake Bell <jake@theunraveler.com>
2016-11-22 09:32:04 -0600

    Adding some logging to debug duplicate order creation

--- app/models/spree/order_updater_decorator.rb
+++ app/models/spree/order_updater_decorator.rb
@@ -1,9 +1,13 @@
 Spree::OrderUpdater.class_eval do
   def update_payment_state_with_create_3plcentral_order
-    initial_state = order.payment_state
-    result = update_payment_state_without_create_3plcentral_order
-    create_3plcentral_order if initial_state != 'paid' && order.payment_state == 'paid'
-    result
+    logger.tagged('3PLCentral', "Order ##{order.number}") do
+      initial_state = order.payment_state
+      logger.debug("Current order state: #{initial_state}")
+      update_payment_state_without_create_3plcentral_order.tap do
+        logger.debug("New order state: #{order.payment_state}")
+        create_3plcentral_order if initial_state != 'paid' && order.payment_state == 'paid'
+      end
+    end
   end
 
   alias_method_chain :update_payment_state, :create_3plcentral_order
